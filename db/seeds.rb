0d746c1cb316bf6ea0ef948ca05713173e83ced3
yshmarov <yshmarov@gmail.com>
2023-09-08 20:04:35 +0200

    create posts. seeds. index and show actions only

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Post.create(title: "a free post", premium: false)
+Post.create(title: "another free post", premium: false)
+Post.create(title: "a premium post", premium: true)
